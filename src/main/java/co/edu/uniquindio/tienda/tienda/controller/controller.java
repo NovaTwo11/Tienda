30989d523626c95df1cd5da0820b16512055a132
NovaTwo11 <holdann.lopezs@gmail.com>
2024-03-05 17:50:21 -0500

    Uno besito otra vez

--- src/main/java/co/edu/uniquindio/tienda/tienda/controller/controller.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.tienda.tienda.controller;
-
-import javafx.event.ActionEvent;
-
-public class controller {
-    public void onHelloButtonClick(ActionEvent actionEvent) {
-
-    }
-}
